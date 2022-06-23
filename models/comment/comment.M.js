062d20eafbdef94dc95bb264878be44c2adf3885
NCV03032001 <canaykholamday@gmail.com>
2022-06-23 16:38:11 +0700

    Thêm phần quản lý comment status. Nhớ chạy lại file db.

--- models/comment/comment.M.js
+++ models/comment/comment.M.js
@@ -12,9 +12,13 @@ exports.getAllComments = async () => {
 exports.getAllCommentsWithAccount = async () => {
   const { rows } =
     await db.query(`select x.username, y.* from ${accounts} 
-  x join ${comments} y on x.account_id = y.account_id`);
+  x join ${comments} y on x.account_id = y.account_id
+  order by y.comment_time desc`);
   return rows;
 };
+exports.updateComment = async (id, field, value) => {
+  await db.query(`update ${comments} set ${field} = ${value} where comment_id = ${id}`);
+}
 exports.deleteComment = async (id) => {
   await db.query(`delete from ${comments} where comment_id=${id}`);
 };
